eda877f3852a9d661f2d1c69da4f28a2ae93c392
artem <artem.kaseka@gmail.com>
2018-12-10 00:59:53 +0200

    started  project

---
+++ src/components/app.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import TodoList from './todo-list';
+import AppHeader from './app-header';
+import SearchPanel from './search-panel';
+
+const App = () => {
+
+    const todoData = [
+        { label: 'Drink coffee', important: false, id: 1 },
+        { label: 'Make awesome app', important: true, id: 2 },
+        { label: 'Have a lunch', important: false, id: 3 }
+    ]
+
+    return (
+        <div>
+            <AppHeader />
+            <SearchPanel />
+            <TodoList todos={ todoData } />
+        </div>
+    )
+};
+
+export default App;
