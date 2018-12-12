5c669bca47881de009441f3f70bdb6de0cd0a501
artem <artem.kaseka@gmail.com>
2018-12-13 00:51:19 +0200

    changed project structure

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
-import TodoList from './todo-list';
-import AppHeader from '../components/app-header';
-import SearchPanel from './search-panel';
+import TodoList from '../todo-list/todo-list';
+import AppHeader from '../app-header';
+import SearchPanel from '../search-panel/search-panel';
+
+import './app.css';
 
 const App = () => {
 
