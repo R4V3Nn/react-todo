92b01475444d27fcc75e57f554e8622261b313f0
Artem <artem.kaseka@gmail.com>
2019-01-29 16:53:38 +0200

    added toggles for important and done task

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,31 +1,133 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import TodoList from '../todo-list';
 import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
-import ItemStatusFilter from '../item-status-filter/';
+import ItemStatusFilter from '../item-status-filter';
+import ItemAddForm from '../item-add-form';
 
 import './app.css';
 
-const App = () => {
+export default class App extends Component {
 
-    const todoData = [
-        { label: 'Drink coffee', important: false, id: 1 },
-        { label: 'Make awesome app', important: true, id: 2 },
-        { label: 'Have a lunch', important: false, id: 3 }
-    ]
+    constructor() {
+        super();
 
-    return (
-        <div className="todo-app">
-          <AppHeader toDo={1} done={3} />
-          <div className="top-panel d-flex">
-            <SearchPanel />
-            <ItemStatusFilter />
-          </div>
-    
-          <TodoList todos={todoData} />
-        </div>
-    )
+        this.maxId = 0;
+
+        this.createTodoItem = (label) => {
+            return {
+                label,
+                important: false,
+                done: false,
+                id: this.maxId++
+            }
+        };
+
+        this.state = {
+
+            todoData: [
+                this.createTodoItem('Drink coffee'),
+                this.createTodoItem('Make something'),
+                this.createTodoItem('Learn React')
+            ]
+        };
+
+
+
+        this.deleteItem = (id) => {
+            this.setState(({todoData}) => {
+                
+                const idx = todoData.findIndex((el) => el.id === id);
+
+                const newArray = [...todoData.slice(0, idx), ...todoData.slice(idx +1)];
+
+                return {
+                    todoData: newArray
+                }
+            })
+        };
+
+        this.addItem = (text) => {
+
+            const newItem = this.createTodoItem(text)
+
+            this.setState(({todoData}) =>{
+                
+                const newArray = [...todoData, newItem];
+                
+                return {
+                    todoData: newArray
+                }
+            })
+        };
+
+        this.toggleProperty = (arr, id, propName) => {
+
+            const idx = arr.findIndex((el) => el.id === id);
+
+                // Update object
+
+                const oldItem = arr[idx];
+                const newItem = {...oldItem, [propName]: !oldItem[propName]};
+
+                // Construcr new Array
+
+                return [
+                    ...arr.slice(0, idx),
+                    newItem,
+                    ...arr.slice(idx + 1)
+                ]
+
+        }
+
+        this.onToggleDone = (id) => {
+
+            this.setState(({todoData}) => {
+                
+                return {
+                    todoData: this.toggleProperty(todoData, id, 'done')
+                }
+
+            })
+        };
+
+        this.onToggleImportant = (id) => {
+
+            this.setState(({todoData}) => {
+                
+                return {
+                    todoData: this.toggleProperty(todoData, id, 'important')
+                }
+
+            })
+        };
+    };
+
+    render() {
+
+        const {todoData} = this.state;
+
+        const doneCount = todoData.filter((el) => el.done).length;
+        const todoCount = todoData.length - doneCount;
+
+        return(
+            <div className = "todo-app" >
+                <AppHeader toDo={todoCount} done={doneCount} />
+                <div className="top-panel d-flex">
+                    <SearchPanel />
+                    <ItemStatusFilter />
+                </div>
+
+                <TodoList
+                    todos={todoData}
+                    onDeleted={this.deleteItem}
+                    onToggleDone={this.onToggleDone}
+                    onToggleImportant={this.onToggleImportant}
+                />
+                <ItemAddForm
+                onItemAdded={this.addItem} />
+            </div>
+        );
+    };
 };
-
-export default App;
--- src/components/todo-list/todo-list.js
+++ src/components/todo-list/todo-list.js
@@ -4,14 +4,20 @@ import TodoListItem from '../todo-list-item/todo-list-item';
 
 import './todo-list.css';
 
-const TodoList = ({ todos }) => {
+const TodoList = ({ todos, onDeleted, onToggleDone, onToggleImportant }) => {
 
     const elements = todos.map((item) => {
 
         const { id, ...itemProps } = item;
 
         return (
-            <li key={ id } className="list-group-item"><TodoListItem {...itemProps} /></li>
+            <li key={ id } className="list-group-item">
+            <TodoListItem
+             {...itemProps}
+             onDeleted={() => onDeleted(id)}
+             onToggleDone={() => onToggleDone(id)}
+             onToggleImportant={() => onToggleImportant(id)}
+              /></li>
         )
     })
 
